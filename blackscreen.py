253d0b1660e532458bda9fe8e1fbe2ce138b9fe2
abinaya2006 <72507845+abinaya2006@users.noreply.github.com>
2021-10-25 22:22:26 +0530

    Add files via upload

---
+++ blackscreen.py
@@ -0,0 +1,33 @@
+import cv2 
+import numpy as np
+
+
+cap=cv2.VideoCapture(0)
+image=cv2.imread("bg.jpg")
+
+while True:
+    ret,frame=cap.read()
+
+    frame=cv2.resize(frame,(640,480))
+    image=cv2.resize(image,(640,480))
+
+    l_black=np.array([104,153,70])
+    u_black=np.array([30,30,0])
+
+    mask=cv2.inRange(frame,l_black,u_black)
+    res=cv2.bitwise_and(frame,frame,mask=mask)
+
+    f=frame-res
+    f=np.where(f==0,image,f)
+
+    cv2.imshow("video",frame)
+    cv2.imshow("mask",f)
+
+    cv2.waitKey(1)
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
